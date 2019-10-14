ccf2b0b5353814816e9c9f044fb712f4f91b145c
Welington da Silva <thebylito@gmail.com>
2019-10-14 13:55:26 -0400

    Fix: Virgula dupla ao salvar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: true,
+        printWidth: 100
+      }
+    ],
+    "comma-dangle": 0,
+  }
 };
