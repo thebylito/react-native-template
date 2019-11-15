d69dfc783c9c46ad592b27327f17dead2ecf1356
Welington da Silva <thebylito@gmail.com>
2019-11-15 10:21:00 -0400

    Atualização de pacotes

--- src/config/reactotronConfig.js
+++ src/config/reactotronConfig.js
@@ -1,9 +1,11 @@
-import Reactotron, {overlay} from 'reactotron-react-native';
-import {reactotronRedux} from 'reactotron-redux';
+import Reactotron, { overlay } from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure()
     .useReactNative()
     .use(reactotronRedux())
     .use(overlay())
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import {persistStore, persistReducer} from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 import Reactotron from 'reactotron-react-native';
 import reducers from 'store/ducks';
@@ -13,7 +13,7 @@ const persistConfig = {
 };
 
 const sagaMonitor = __DEV__ ? Reactotron.createSagaMonitor() : null;
-const sagaMiddleware = createSagaMiddleware({sagaMonitor});
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 const middlewares = [sagaMiddleware];
@@ -21,7 +21,7 @@ const middlewares = [sagaMiddleware];
 const createCompose = __DEV__
   ? compose(
       applyMiddleware(...middlewares),
-      Reactotron.createEnhancer(),
+      Reactotron.createEnhancer()
     )
   : compose(applyMiddleware(...middlewares));
 
@@ -30,5 +30,5 @@ export default () => {
   const store = createStore(persistedReducer, createCompose);
   const persistor = persistStore(store);
   sagaMiddleware.run(rootSaga);
-  return {store, persistor};
+  return { store, persistor };
 };
