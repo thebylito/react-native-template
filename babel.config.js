fabed57b39fe7ea628182e98fc1cd19a15e67b9a
Welington da Silva <thebylito@gmail.com>
2019-12-02 17:20:18 -0400

    Atualização de pacotes
    - Adicionado plugin para Babel, optional-chaining

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-      },
-    ],
-  ],
+  plugins: ['@babel/plugin-proposal-optional-chaining', ['module-resolver', { root: ['./src'] }]],
 };
