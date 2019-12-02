c317deff31c242e1863d66ef2befcd5b4a3c48f6
Welington da Silva <thebylito@gmail.com>
2019-12-02 17:19:06 -0400

    Adicionado arquivos de utilidades(cores e métricas)

---
+++ src/utils/appColors.js
@@ -0,0 +1,13 @@
+export const appColors = {
+  primary: '#228B22',
+  secondary: '#7D451B',
+
+  black: 'black',
+  white: '#fff',
+  gray: 'gray',
+  ligthGray: '#D8D8D8',
+  transparent: 'transparent',
+  red: 'red',
+};
+
+export default appColors;
