a6ab76131ae71ff97bd591e42f530e537b17d859
Welington da Silva Martins <thebylito@gmail.com>
2020-03-31 13:36:43 -0300

    Atualizado configuracoes de Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': [
       'error',
       {
         trailingComma: 'es5',
-        singleQuote: true,
-      },
+        singleQuote: true
+      }
     ],
-    'comma-dangle': 0,
-  },
+    'comma-dangle': 0
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   eslintIntegration: true,
   arrowParens: 'avoid',
 };
