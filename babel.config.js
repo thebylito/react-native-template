c37b5e60e15e5a4ea190e651885929a3f0cc7a1b
Welington da Silva Martins <thebylito@gmail.com>
2020-11-23 08:55:18 -0300

    Atualizacao react-native
    - Atualizacao de dependencias
    - Adicionado suporte ao typescript

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     '@babel/plugin-proposal-optional-chaining',
-    ['module-resolver', { root: ['./src'] }],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
   ],
 };
